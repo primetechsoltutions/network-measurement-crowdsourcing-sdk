bcea3cabff3aa3be0ba8c51f218614733d8a3b6d
abusufianshibli <primetechsolutiong@gmail.com>
2026-02-28 01:47:22 +0600

    unified sdk with rtt,latency,location gps issue, ftp dl,ul volume,and ftp network data measurement and capture for bl wifi device assessment.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,10 +21,6 @@ dependencyResolutionManagement {
 //            url = uri("C:\\Users\\Sazzad_PTSL\\Desktop\\MyBL SDK\\mybl-network-measurement-sdk\\network-sdk\\build\\outputs\\aar")
 //            url = uri("/Users/shebleredwan/Desktop/network_measurement_event_base_sdk/network-sdk/build/outputs/aar")
 
-//            credentials {
-//                username ="primetechsoltutions"
-//                password = "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"
-//            }
         }
 
 //        maven {
