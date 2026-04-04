54adfbec29d8c7fff4fdadc14cc6658ac18781d8
abusufianshibli <primetechsolutiong@gmail.com>
2026-04-04 13:11:15 +0600

    Pre-Release-v-1.0.5 test.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(project(mapOf("path" to ":network-sdk")))
-//    implementation("com.github.primetechsoltutions:network-measurement-unified-sdk:Test-1.0.2")
-//    implementation("com.github.primetechsoltutions:network-measurement-unified-sdk:Live-1.0.1")
+//    implementation("com.github.primetechsoltutions:network-measurement-unified-sdk:Test-1.0.4")
+//    implementation("com.github.primetechsoltutions:network-measurement-unified-sdk:Live-1.0.4")
 
     implementation(libs.play.services.location)
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
